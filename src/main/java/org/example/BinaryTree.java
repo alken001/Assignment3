e3c052e8b33adb36f38b45ffd2303fdf48f1fb2b
zaira <231128@astanait.edu.kz>
2024-05-14 21:23:36 +0500

    add min, max, insert functions

--- src/main/java/org/example/BinaryTree.java
+++ src/main/java/org/example/BinaryTree.java
@@ -35,4 +35,37 @@ public class BinaryTree {
         return root;
     }
 
+    public void delete(int key) {
+        root = deleteRec(root, key);
+    }
+
+    private Node deleteRec(Node root, int key) {
+        if (root == null) return root;
+
+        if (key < root.key)
+            root.left = deleteRec(root.left, key);
+        else if (key > root.key)
+            root.right = deleteRec(root.right, key);
+        else {
+            if (root.left == null)
+                return root.right;
+            else if (root.right == null)
+                return root.left;
+
+            root.key = minValue(root.right);
+
+            root.right = deleteRec(root.right, root.key);
+        }
+
+        return root;
+    }
+
+    private int minValue(Node root) {
+        int minv = root.key;
+        while (root.left != null) {
+            minv = root.left.key;
+            root = root.left;
+        }
+        return minv;
+    }
 }
--- src/main/java/org/example/Hash.java
+++ src/main/java/org/example/Hash.java
@@ -16,4 +16,58 @@ class HashTable {
     private int hash(int key) {
         return key % SIZE;
     }
+
+    void insert(int key) {
+        int index = hash(key);
+        table[index].add(key);
+    }
+
+    void delete(int key) {
+        int index = hash(key);
+        table[index].remove(Integer.valueOf(key));
+    }
+
+    void display() {
+        for (int i = 0; i < SIZE; i++) {
+            System.out.print("Bucket " + i + ": ");
+            for (Integer item : table[i]) {
+                System.out.print(item + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    int sum() {
+        int sum = 0;
+        for (int i = 0; i < SIZE; i++) {
+            for (Integer item : table[i]) {
+                sum += item;
+            }
+        }
+        return sum;
+    }
+
+    int max() {
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < SIZE; i++) {
+            for (Integer item : table[i]) {
+                if (item > max) {
+                    max = item;
+                }
+            }
+        }
+        return max;
+    }
+
+    int min() {
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < SIZE; i++) {
+            for (Integer item : table[i]) {
+                if (item < min) {
+                    min = item;
+                }
+            }
+        }
+        return min;
+    }
 }
